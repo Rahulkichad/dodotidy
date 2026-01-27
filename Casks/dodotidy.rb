33add2c5165eb749faf503b9995078e6c3583cd9
gorkem-bwl <gorkem.cetin@bluewavelabs.ca>
2026-01-27 00:22:34 -0500

    Bump version to 1.0.1 for Turkish localization release

--- Casks/dodotidy.rb
+++ Casks/dodotidy.rb
@@ -1,6 +1,6 @@
 cask "dodotidy" do
-  version "1.0.0"
-  sha256 "ec6ae05553c5a25ae1fd12cb6faf791202a9312f2b98b1fc61909cb0d30298ec"
+  version "1.0.1"
+  sha256 "3fc3ed8753f82a9254cdd99c5068c727be76d964ff1d2460ddbb9d8682ae5118"
 
   url "https://github.com/bluewave-labs/DodoTidy/releases/download/v#{version}/DodoTidy-#{version}.dmg"
   name "DodoTidy"
